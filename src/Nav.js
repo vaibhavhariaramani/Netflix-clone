8970b2ea4de9b54800da7619a3cfb41da5263bbe
unknown <vaibhav.hariramani01@gmail.com>
2022-09-03 20:31:35 +0530

    uploading complete files

---
+++ src/Nav.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import "./Nav.css";
+
+function Nav() {
+  const [show,handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll",()=>{
+      if(window.scrollY >100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+
+    return () => {
+      window.removeEventListener("scroll",null);
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+        <img
+            className='nav_logo'
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Logonetflix.png/800px-Logonetflix.png"
+            alt="Netflix Logo"
+        />
+
+        <img
+            className='nav_avatar'
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117"
+            alt='Netflix Logo'
+        />
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "b5bbdc8beca01c3d0da5590e12e10ea5";
+
+const requests = {
+    fetchTrending:`/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchingNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
